840422e58b7f09bcd4341d89e13b7fb3ea7a10cb
AirMaks <rm1010@yandex.ru>
2022-07-18 16:46:13 +0300

    Infinity to max safe integer

--- index.ts
+++ index.ts
@@ -9,14 +9,16 @@ export default function filter(
 ): ICourses[] {
   return courses.filter((course) => {
     const coursePriceFrom = course.prices[0] ?? 0;
-    const coursePriceTo = course.prices[1] ?? Infinity;
+    const coursePriceTo = course.prices[1] ?? Number.MAX_SAFE_INTEGER;
 
     const from = prices[0] ?? 0;
-    const to = prices[1] ?? Infinity;
+    const to = prices[1] ?? Number.MAX_SAFE_INTEGER;
 
-    if (coursePriceFrom === 0 && coursePriceTo === Infinity) return null;
+    if (coursePriceFrom === 0 && coursePriceTo === Number.MAX_SAFE_INTEGER)
+      return null;
 
     return from <= coursePriceTo && to >= coursePriceFrom;
   });
 }
+
 
