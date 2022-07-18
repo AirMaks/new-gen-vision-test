f0b5b7c844c6ae997e8894e5ac6a50b6fbd96147
AirMaks <rm1010@yandex.ru>
2022-07-18 16:26:46 +0300

    new version

--- index.ts
+++ index.ts
@@ -1,26 +1,3 @@
-function between(
-  x: number | null,
-  y: number | null,
-  min: number | null,
-  max: number | null
-): boolean {
-  // if user inputs nothing return all courses
-  if (x === null && y === null) return true;
-
-  // if course doesn't have prices don not show this course
-  if (min === null && max === null) return false;
-
-  // if user search price from
-  if (y === null && max === null) return x >= min;
-  if (y === null && min === null) return x <= max;
-  if (y === null && max !== null) return x <= max && (x >= min || x <= max);
-
-  // if user search price to
-  if (x === null) return y >= min;
-
-  return (max === null && y >= min) || (max !== null && x <= max && y >= min);
-}
-
 interface ICourses {
   name: string;
   prices: (number | null)[];
@@ -28,17 +5,21 @@ interface ICourses {
 
 export default function filter(
   courses: ICourses[],
-  pricesArr: (number | null)[]
+  prices: (number | null)[]
 ): ICourses[] {
-  let filteredResult: ICourses[] = [];
-  courses.forEach(function (course) {
-    if (
-      between(pricesArr[0], pricesArr[1], course.prices[0], course.prices[1])
-    ) {
-      filteredResult.push(course);
-    }
-  });
-  return filteredResult;
-}
+  return courses.filter((course) => {
+    const coursePriceFrom = course.prices[0] ?? 0;
+    const coursePriceTo = course.prices[1] ?? Infinity;
 
+    const from = prices[0] ?? 0;
+    const to = prices[1] ?? Infinity;
+
+    if (coursePriceFrom === 0 && coursePriceTo === Infinity) return null;
+
+    return (
+      (from === coursePriceFrom && from <= coursePriceFrom) ||
+      (from <= coursePriceTo && to >= coursePriceFrom)
+    );
+  });
+}
 
